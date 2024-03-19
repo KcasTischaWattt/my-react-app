fb06f8879afdc8c3b0d8e13c251a5f4eca792913
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-19 21:48:38 +0300

    Added sorting selector functionality

--- src/CardList/index.js
+++ src/CardList/index.js
@@ -1,16 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import '../App.css';
 import { Card } from '../Card';
 import { GlobalContext } from '../App';
 
 export const CardList = () => {
+  const [orderBy, setOrderBy] = useState('desc');
   const { state } = useContext(GlobalContext);
+  let sortedCitiesList = [...state.citiesList];
+  if (orderBy === 'asc') {
+    sortedCitiesList = state.citiesList.sort();
+  } else {
+    sortedCitiesList = state.citiesList.sort().reverse();
+  }
+
+  const handleOnChange = (e) => {
+    setOrderBy(e.target.value);
+  }
+
   return (
     <>
-      <select class = "Select">
-        <option value="name">Name</option>
-        <option value="temperature">Temperature</option>
+      <select class = "Select" value={orderBy} onChange={handleOnChange}>
+        <option value="desc">By name desc</option>
+        <option value="asc">By name asc</option>
       </select>
       <div className='CardList'>
         {
