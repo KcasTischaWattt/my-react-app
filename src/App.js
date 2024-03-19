069cc4286353733ffa3fc788f0fe33124a3f43b2
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-19 11:34:48 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Main">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/input/index.js
@@ -0,0 +1,10 @@
+import '../App.css';
+
+export const Input = () => {
+  return (
+    <div className="Main">
+    </div>
+  );
+}
+
+export default App;
