7b07656aeb5d3ce940e534bb8b24fd00eb5b1d15
KcasTischaWattt <tmnbob2012@gmail.com>
2024-03-19 14:00:06 +0300

    Added default event handlers

--- src/input/index.js
+++ src/input/index.js
@@ -3,10 +3,24 @@ import React from 'react';
 import '../App.css';
 
 export const Input = () => {
+  const currentDate = new Date().toDateString();
+  const handleOnChange = (event) => {
+      console.log(event.target.value);
+  }
+  const handleOnClick = (value) => () => {
+      console.log('clicked');
+      console.log(value);
+  }
+  const handleOnSubmit = (event) => {
+      event.preventDefault();
+      console.log('submitted');
+  } 
   return (
     <div className="InputWrap">
-        <input className="Input" />
-        <button className="Button">+</button>
+        <form onSubmit={handleOnSubmit}>
+          <input className="Input" onChange={handleOnChange} />
+          <button className="Button" onClick={handleOnClick(currentDate)}>+</button>
+        </form>
     </div>
   );
 }
